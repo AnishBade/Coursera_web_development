ba54e5209f5b7ae7fd29133a0296774ca3592e41
anish <badeanish6045@gmail.com>
2020-04-21 23:51:59 +0545

    4/21

--- COURSERA_JS/events.js
+++ COURSERA_JS/events.js
@@ -7,3 +7,56 @@ function displayDate()
 {
     document.getElementById("paragraph").innerHTML='The date is: '+  Date();
 }
+
+function change(msg)
+{
+    document.getElementById("changeable").innerHTML=msg +' button is clicked.';
+}
+
+
+
+
+function openMe()
+{
+   op=document.getElementById("paragraph1");
+   /* we can change the style attribute directly
+ op.style.display="block";
+     */
+  //or we can create a class to change the style from css
+  op.className="open";
+    
+}
+
+function closeMe()
+{
+     clo=document.getElementById("paragraph1");
+    
+   /* we can change the style attribute directly
+
+  clo.style.display="none"; */
+   //or we can create a class to change the style from css
+    clo.className="close";
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
